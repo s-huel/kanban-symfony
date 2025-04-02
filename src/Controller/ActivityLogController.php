3acc245a774c7a218a8e324afe550289457e7572
Serena Huel <felix.huel6@gmail.com>
2025-04-02 18:38:37 +0200

    fix: security web page

--- src/Controller/ActivityLogController.php
+++ src/Controller/ActivityLogController.php
@@ -8,8 +8,10 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/api/activity-logs')]
+#[IsGranted('ROLE_USER')]
 class ActivityLogController extends AbstractController
 {
     private ActivityLogService $activityLogService;
--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -14,8 +14,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/api/task')]
+#[IsGranted('ROLE_USER')]
 class TaskController extends AbstractController
 {
     private ActivityLogService $activityLogService;
