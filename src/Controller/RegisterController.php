4488e6053f637a04f267a32718ef95217e92d57d
Serena Huel <felix.huel6@gmail.com>
2025-04-04 12:45:47 +0200

    fix: registration change and twig add

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,47 +2,40 @@
 
 namespace App\Controller;
 
-use App\DTO\RegistrationRequestDTO;
 use App\Entity\User;
+use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class RegisterController extends AbstractController
 {
-    #[Route('/api/register', name: 'register', methods: ['POST'])]
-    public function register(
-        #[MapRequestPayload] RegistrationRequestDTO $registrationRequest,
+    #[Route('/register', name: 'register', methods: ['GET', 'POST'])]
+    public function registerForm(
+        Request $request,
         UserPasswordHasherInterface $passwordHasher,
-        EntityManagerInterface $entityManager,
-        ValidatorInterface $validator
-    ): JsonResponse {
-        $errors = $validator->validate($registrationRequest);
-        if (count($errors) > 0) {
-            return $this->json($errors, 400);
-        }
-
-        if ($entityManager->getRepository(User::class)->findOneBy(['email' => $registrationRequest->email])) {
-            return $this->json(['error' => 'Email already exists'], 400);
-        }
-
+        EntityManagerInterface $entityManager
+    ): Response {
         $user = new User();
-        $user->setEmail($registrationRequest->email);
-        $user->setPassword(
-            $passwordHasher->hashPassword($user, $registrationRequest->password)
-        );
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
 
-        $entityManager->persist($user);
-        $entityManager->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordHasher->hashPassword($user, $form->get('plainPassword')->getData())
+            );
 
-        return $this->json([
-            'id' => $user->getId(),
-            'email' => $user->getEmail(),
-            'roles' => $user->getRoles()
-        ], 201);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->render('register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
     }
 }
